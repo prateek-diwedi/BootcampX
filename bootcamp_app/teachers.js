97f8d30fbc5b2905a4d87740787bdcf02d146712
Prateek Diwedi <diwedi1112@gmail.com>
2020-01-09 05:08:18 +0000

    updated hack free code for student and teacher files

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,19 +6,24 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohortName = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
 
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort} : ${row.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+`;
+
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort} : ${row.teacher}`);
+    })
+  }).catch(err => console.error('query error', err.stack));
